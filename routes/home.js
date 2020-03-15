98e7882676352e273ed0e4c5f516d2030d23f085
danielam <danielam313101@gmail.com>
2020-03-15 23:24:01 +0800

    類別

--- routes/home.js
+++ routes/home.js
@@ -4,9 +4,18 @@ const Record = require('../models/record')
 const { authenticated } = require('../middleware/auth')
 
 router.get('/', authenticated, (req, res) => {
-  Record.find({user: req.user._id})
+  const { category } = req.query
+  const categoryFilter = category || ''
+  Record.find({user: req.user._id, category: {
+    $regex: categoryFilter, $options:'i'
+  }})
     .lean()
-    .then(records => res.render('index', {records}))
+    .then(records => {
+    let totalAmount = 0
+    if (records.length > 0) {
+      totalAmount = records.map(record => parseInt(record.amount)).reduce((a, b) => a + b)
+    }
+      return res.render('index', {records, totalAmount, category: category ? category : '不設類別'})})
     .catch(err => {
       let errors = []
       errors.push({ message: `發生錯誤: ${err}` })
